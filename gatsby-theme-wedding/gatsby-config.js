4856a619bf06e9743ce3fea0eb713d506537c24b
Agney <agney@outlook.in>
2019-11-10 11:15:55 +0530

    add necessary packages

--- gatsby-theme-wedding/gatsby-config.js
+++ gatsby-theme-wedding/gatsby-config.js
@@ -1,16 +1,21 @@
 module.exports = {
-    plugins: [
-      {
-        resolve: "gatsby-source-filesystem",
-        options: {
-          path: "data",
-        },
-      },
-      {
-        resolve: "gatsby-transformer-json",
-        options: {
-          typeName: "Event",
-        },
-      },
-    ],
-  }
+  siteMetadata: {
+    title: "Something & Something",
+    description: "Something",
+    author: "Agney Menon"
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: "data"
+      }
+    },
+    {
+      resolve: "gatsby-transformer-json",
+      options: {
+        typeName: "Event"
+      }
+    }
+  ]
+};
